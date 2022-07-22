2f5560acd0d421dfe60a8e0750afa81642af7230
eunji0828 <elj0828@hanmail.net>
2022-07-22 19:43:32 +0900

    220722 이은지

---
+++ signUp.js
@@ -0,0 +1,12 @@
+//회원가입 정규표현식
+// name // 이름
+// id // 아이디
+// password // 비밀번호
+// birth // 생년월일
+// gender // 성별
+// phoneNumber // 핸드폰번호
+// address // 주소
+// email // 이메일
+// signupDate // 회원가입 일시
+// lastlogin // 마지막 로그인
+
